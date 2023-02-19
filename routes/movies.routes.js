34e770cb0668c032477cb2ccce3d508611ece828
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-02-19 21:36:35 +0100

    lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -30,6 +30,53 @@ router.post("/create", (req, res, next) => {
       })
       .catch(err => { next(err) })
 })
+
+router.get("/:id", (req, res, next) => {
+  let movieId = req.params.id
+  Movie.findById(movieId)
+  .populate("cast")
+  .then(result => {
+    res.render("movies/movie-details", {result})
+  })
+  .catch(err => { next(err) })
+})
+
+router.post("/:id/delete", (req, res, next) => {
+  let deleteId = req.params.id
+  Movie.findByIdAndRemove(deleteId)
+    .then(() => {
+      res.redirect("/movies")
+    })
+    .catch(err => { next(err) })
+    
+})
+
+router.get("/:id/edit", (req, res, next) => {
+  let movieId = req.params.id
+  Movie.findByIdAndUpdate(movieId)
+  .then(result => {
+    let movie = result;
+    Celebrity.find()
+    .then(result => {
+      let celebrities = result
+      res.render("movies/edit-movie",  {movie, celebrities})
+    })
+  })
+  .catch(err => { next(err) })
+})
+
+router.post("/:id/edit", (req, res, next) => {
+  const {title, genre, plot, cast} = req.body
+  let movieId = req.params.id
+  Movie.findByIdAndUpdate(movieId, {title, genre, plot, cast})
+  .then(result => {
+    res.redirect(`/movies/${movieId}`)
+  })
+  .catch(err => { next(err) })
+})
+
+
+
   
 
 module.exports = router;
